f70dad986128718fd6384b77c9b047e9344bda12
Hidenori Miyoshi <miyohide@gmail.com>
2014-05-22 22:35:14 +0900

    print name, parameter, max, min memory(avg)

--- lib/rbs_yaml_parse.rb
+++ lib/rbs_yaml_parse.rb
@@ -1,3 +1,4 @@
 require 'rbs_yaml_parse/version'
 require 'rbs_yaml_parse/cli'
+require 'csv'
 
--- lib/rbs_yaml_parse/cli.rb
+++ lib/rbs_yaml_parse/cli.rb
@@ -14,10 +14,24 @@ module RbsYamlParse
 
          exec_info = parse_results.shift
 
+         print_list = []
          parse_results.each do |parse_result|
-            puts parse_result["memory_usages"].inspect
+            next if parse_result.has_key?("status")
+
+            one_line = []
+            one_line << parse_result["name"]
+            one_line << parse_result["parameter"]
+            one_line << parse_result["max"]
+            one_line << parse_result["min"]
+
+            memory_usages = parse_result["memory_usages"]
+            one_line << memory_usages.inject(0) { |i, sum| i + sum } / memory_usages.size
+            print_list << one_line
          end
 
+         print_list.each do |line|
+            print line.to_csv
+         end
          0
       end
 
