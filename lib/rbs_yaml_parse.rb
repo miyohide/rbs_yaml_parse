336548f142882764a40131cb8a6f389a58b92634
Hidenori Miyoshi <miyohide@gmail.com>
2014-05-28 22:33:25 +0900

    implement option_parse

--- lib/rbs_yaml_parse.rb
+++ lib/rbs_yaml_parse.rb
@@ -1,4 +1,5 @@
 require 'rbs_yaml_parse/version'
 require 'rbs_yaml_parse/cli'
+require 'rbs_yaml_parse/option_parse'
 require 'csv'
 
---
+++ lib/rbs_yaml_parse/option_parse.rb
@@ -0,0 +1,31 @@
+# coding: utf-8
+
+require 'optparse'
+
+module RbsYamlParse
+   class OptionParse
+      def initialize(args)
+         @args = args
+      end
+
+      def parse
+         params = {}
+
+         opt = OptionParser.new
+         opt.on('-d dirname') { |v| params[:d] = v }
+         opt.on('-a') { |v| params[:a] = v }
+         opt.on('--maxmem') { |v| params[:maxmem] = v }
+         opt.on('--minmem') { |v| params[:minmem] = v }
+         opt.on('--avgmem') { |v| params[:avgmem] = v }
+
+         opt.on('--maxtime') { |v| params[:maxtime] = v }
+         opt.on('--mintime') { |v| params[:mintime] = v }
+         opt.on('--avgtime') { |v| params[:avgtime] = v }
+
+         opt.parse!(@args)
+
+         params
+      end
+   end
+end
+
