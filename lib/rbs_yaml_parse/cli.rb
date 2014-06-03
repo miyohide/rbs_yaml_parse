f4c99767b4fa88c4141e1157b5bd6bc7fa1b6967
Hidenori Miyoshi <miyohide@gmail.com>
2014-06-03 23:18:21 +0900

    とりあえずminの値を出すことが出来た

--- lib/rbs_yaml_parse/cli.rb
+++ lib/rbs_yaml_parse/cli.rb
@@ -1,41 +1,41 @@
 module RbsYamlParse
    class CLI
 
-      def initialize(args)
-         @filename = args.size > 0 ? args[0] : nil
+      def initialize(command_args)
+         @option_parser = OptionParse.new(command_args)
       end
 
-      def run
-         f = File.open(@filename)
-         parse_results = YAML.load_stream(f)
-         f.close
+      def run!
+         params = @option_parser.parse
 
-         exec_info = parse_results.shift
+         filepath = File.join(params[:d], "**/*.yaml")
+         yaml_files = Dir.glob(filepath).to_a.sort
 
-         print_list = []
-         print_list << header_list
-         parse_results.each do |parse_result|
-            next if parse_result.has_key?("status")
+         data = Hash.new { |h,k| h[k] = {} }
 
-            one_line = []
-            one_line << parse_result["name"]
-            one_line << parse_result["parameter"]
-            one_line << parse_result["max"]
-            one_line << parse_result["min"]
+         yaml_files.each do |yaml_file|
+            File.open(yaml_file) { |file|
+               YAML.load_documents(file) do |doc|
+                  next if doc["min"].nil?
+                  key = [File.basename(doc["name"]), doc["parameter"]]
 
-            memory_usages = parse_result["memory_usages"]
-            one_line << memory_usages.inject(0) { |i, sum| i + sum } / memory_usages.size
-            print_list << one_line
+                  data[key][yaml_file]    = doc["min"]
+               end
+            }
          end
 
-         print_list.each do |line|
-            print line.to_csv
-         end
-         0
-      end
+         data.keys.each do |k|
+            one_line = k.join(",")
 
-      def header_str
-         "name,parameter,max,min,memory(avg)\n"
+            one_line = yaml_files.inject(one_line + ",") do |sum, yaml_file|
+               sum + data[k][yaml_file].to_s + ","
+            end
+
+            one_line.gsub!(/,+\Z/, '')
+
+            puts one_line
+
+         end
       end
    end
 end
