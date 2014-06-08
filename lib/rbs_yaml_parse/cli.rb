cfc623800f228e116f65209d6eef2afd5ad3f04e
Hidenori Miyoshi <miyohide@gmail.com>
2014-06-09 00:03:59 +0900

    テストを通すように実装

--- lib/rbs_yaml_parse/cli.rb
+++ lib/rbs_yaml_parse/cli.rb
@@ -3,8 +3,11 @@ require 'yaml'
 module RbsYamlParse
    class CLI
 
+      attr_reader :data
+
       def initialize(command_args)
          @option_parser = OptionParse.new(command_args)
+         @data = Hash.new { |h,k| h[k] = {} }
       end
 
       def run!
@@ -13,30 +16,27 @@ module RbsYamlParse
          filepath = File.join(params[:d], "**/*.yaml")
          yaml_files = Dir.glob(filepath).to_a.sort
 
-         data = Hash.new { |h,k| h[k] = {} }
-
          yaml_files.each do |yaml_file|
             File.open(yaml_file) { |file|
                YAML.load_documents(file) do |doc|
                   next if doc["min"].nil?
                   key = [File.basename(doc["name"]), doc["parameter"]]
 
-                  data[key][yaml_file]    = doc["min"]
+                  @data[key][yaml_file]    = doc["min"]
                end
             }
          end
 
-         data.keys.each do |k|
+         @data.keys.each do |k|
             one_line = k.join(",")
 
             one_line = yaml_files.inject(one_line + ",") do |sum, yaml_file|
-               sum + data[k][yaml_file].to_s + ","
+               sum + @data[k][yaml_file].to_s + ","
             end
 
             one_line.gsub!(/,+\Z/, '')
 
             puts one_line
-
          end
       end
    end
