aae659fec6e707e96b03c3d71df347925699286e
Hidenori Miyoshi <miyohide@gmail.com>
2014-06-09 23:10:32 +0900

    最小時間・最大時間・平均時間を得られるようにした

--- spec/lib/rbs_yaml_parse/cli_spec.rb
+++ spec/lib/rbs_yaml_parse/cli_spec.rb
@@ -26,7 +26,6 @@ describe RbsYamlParse::CLI do
 
       describe "YAML parse" do
          before do
-            @cli = RbsYamlParse::CLI.new(%w(-d foo))
             lines = <<EOS
 ---
 name: /home/miyoshi/ruby-benchmark-suite/benchmarks/macro-benchmarks/bm_gzip.rb
@@ -42,14 +41,65 @@ min: 7.693091928
 median: 7.698669317
 mean: 7.7073879756
 standard_deviation: 0.01968369053890256
+times:
+- 7.745933742
+- 7.694599331
+- 7.693091928
+- 7.70464556
+- 7.698669317
+memory_usages:
+- 43364352
+- 51826688
+- 54857728
+- 59854848
+- 62676992
 EOS
             Dir.stub_chain(:glob, :to_a, :sort).and_return(["hoge.yaml"])
             File.stub(:open).and_yield(StringIO.new(lines))
          end
 
-         it "yaml parse result" do
-            @cli.run!
-            expect(@cli.data).to eq({["bm_gzip.rb", 100] => {"hoge.yaml" => 7.693091928}})
+         context "option is --mintime" do
+            before do
+               @cli = RbsYamlParse::CLI.new(%w(-d foo --mintime))
+            end
+
+            it "print min time" do
+               @cli.run!
+               expect(@cli.data).to eq({["bm_gzip.rb", 100] => {"hoge.yaml" => { min: 7.693091928 }}})
+            end
+         end
+
+         context "option is --maxtime" do
+            before do
+               @cli = RbsYamlParse::CLI.new(%w(-d foo --maxtime))
+            end
+
+            it "print max time" do
+               @cli.run!
+               expect(@cli.data).to eq({["bm_gzip.rb", 100] => {"hoge.yaml" => { max: 7.745933742 }}})
+            end
+         end
+
+         context "option is --avgtime" do
+            before do
+               @cli = RbsYamlParse::CLI.new(%w(-d foo --avgtime))
+            end
+
+            it "print avg time" do
+               @cli.run!
+               expect(@cli.data).to eq({["bm_gzip.rb", 100] => {"hoge.yaml" => { avg: 7.7073879756 }}})
+            end
+         end
+
+         context "option is --maxtime and --mintime" do
+            before do
+               @cli = RbsYamlParse::CLI.new(%w(-d foo --maxtime --mintime))
+            end
+
+            it "print min and max time" do
+               @cli.run!
+               expect(@cli.data).to eq({["bm_gzip.rb", 100] => {"hoge.yaml" => { min: 7.693091928, max: 7.745933742 }}})
+            end
          end
       end
 
