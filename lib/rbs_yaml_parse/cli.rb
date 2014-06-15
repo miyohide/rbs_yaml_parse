b777323982d60500f34515ef653c66eb9545fba3
Hidenori Miyoshi <miyohide@gmail.com>
2014-06-15 21:53:14 +0900

    ファイル出力機能を実装

--- lib/rbs_yaml_parse/cli.rb
+++ lib/rbs_yaml_parse/cli.rb
@@ -9,12 +9,13 @@ module RbsYamlParse
          @option_parser = OptionParse.new(command_args)
          @data = Hash.new { |h,k| h[k] = {} }
          @yaml_files = []
+         @params = []
       end
 
       def run!
-         params = @option_parser.parse
+         @params = @option_parser.parse
 
-         filepath = File.join(params[:d], "**/*.yaml")
+         filepath = File.join(@params[:d], "**/*.yaml")
          @yaml_files = Dir.glob(filepath).to_a.sort
 
          @yaml_files.each do |yaml_file|
@@ -23,12 +24,12 @@ module RbsYamlParse
                   next if doc["min"].nil?
                   key = [File.basename(doc["name"]), doc["parameter"]]
 
-                  @data[key][yaml_file] = val_map(doc, params)
+                  @data[key][yaml_file] = val_map(doc, @params)
                end
             }
          end
 
-         output_data(params)
+         #output_data(@params)
       end
 
       def val_map(yaml_doc, params)
@@ -72,16 +73,38 @@ module RbsYamlParse
       end
 
       def create_one_line(key, data_key)
-         one_line = data_key.join(",")
+         key_line = data_key.join(",")
 
-         one_line = @yaml_files.inject(one_line + ",") do |sum, yaml_file|
-            sum + @data[data_key][yaml_file][key.to_sym].to_s + ","
+         data_line = @yaml_files.inject("") do |sum, yaml_file|
+            unless @data[data_key][yaml_file].nil?
+               "#{sum}#{@data[data_key][yaml_file][key.to_sym].to_s},"
+            end
          end
 
          # delete last comma(,)
-         one_line.gsub(/,+\Z/, '')
+         "#{key_line},#{data_line}".gsub(/,+\Z/, '')
       end
 
+      def csv_header(val)
+         "program,params,#{val}"
+      end
+
+      def file_output
+         params_keys = %w[mintime maxtime avgtime minmem maxmem avgmem]
+
+         params_keys.each do |params_key|
+            file_name = "result_#{params_key}.csv"
+            File.open(file_name, "w") { |file|
+               file.write "#{csv_header(params_key)}\n"
+               next unless @params.has_key?(params_key.to_sym)
+
+               @data.keys.each do |data_key|
+                  file.write "#{create_one_line(params_key, data_key)}\n"
+               end
+            }
+         end
+
+      end
    end
 end
 
